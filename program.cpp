087c9d2c666fd0cf9c2fbc401160604fd3c0ed49
Jake <wekkaj@gmail.com>
2017-05-16 00:34:05 +0000

    Added derivative and integral components to movement.

--- program.cpp
+++ program.cpp
@@ -22,10 +22,15 @@ class sensorTestResult {
 		}
 };
 
+int delay = 100000;
 int cameraWidth = 320;
 int cameraHeight = 240;
 int motorLeft = 1;
 int motorRight = 2;
+double kp = 1.0;
+double kd = 1.0;
+double ki = 1.0;
+double totalError = 0.0;
 
 void set_movement(double direction, bool reverse) {
 	double left = (int)(255.0 * (direction + 1.0) / 2.0);
@@ -45,13 +50,13 @@ void set_movement(double direction, bool reverse) {
 	}
 }
 
-sensorTestResult get_white_position() {
+sensorTestResult get_white_position(int row) {
 	take_picture();
 
 	double sum = 0.0;
 	int numOfWhite = 0;
 	for (int i = 0; i < cameraWidth; i++) {
-		char white = get_pixel(cameraHeight / 2, i, 3);
+		char white = get_pixel(row, i, 3);
 		if (white > 127) {
 			sum += i - cameraWidth / 2.0;
 			numOfWhite++;
@@ -74,17 +79,26 @@ int main() {
   init();
   
   while (true) {
-    sensorTestResult result = get_white_position();
+    sensorTestResult result = get_white_position(cameraHeight / 2);
+    sensorTestResult previousResult = get_white_position(cameraHeight * 3 / 4);
+    
+    double positionSignal = result.get_white_position() * kp;
+    
+    double difference = result.get_white_position() - previousResult.get_white_position();
+    double derivativeSignal = (difference / (delay / 1000000.0)) * kd;
+    
+    totalError += result.get_white_position();
+    double integralSignal = totalError * ki;
     
     if (result.get_num_of_white() == 0) {
 		set_movement(0.25, true);
 	}
 	else {
-		set_movement(result.get_white_position(), false);
+		set_movement(positionSignal + derivativeSignal + integralSignal, false);
 	}
     
     printf("%f\n", result.get_white_position());
-    sleep1(0, 100000);
+    sleep1(0, delay);
   }
   
   reset();
